f1fc5f6b45bf7da5cc16836bb4ebac5ede6001dd
maiallam57 <maimallam57@gmail.com>
2024-08-23 00:08:23 +0300

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./shared/components/footer/footer.component";
+import { FlowbiteService } from './shared/services/flowbit/flowbit.service';
+import { NavbarComponent } from "./shared/layout/navbar/navbar.component";
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'FreshCart';
+  constructor(private _flowbiteService: FlowbiteService) { }
+
+  ngOnInit(): void {
+    this._flowbiteService.loadFlowbite(flowbite => {
+    });
+  }
 }
