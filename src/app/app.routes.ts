6818ac5220d0548dabb8d07f638bccb1c221f8d6
maiallam57 <maimallam57@gmail.com>
2024-08-27 14:15:02 +0300

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/pages/home/home.component';
+import { CartComponent } from './features/pages/cart/cart.component';
+import { ProductsComponent } from './features/pages/products/products.component';
+import { CategoriesComponent } from './features/pages/categories/categories.component';
+import { BrandsComponent } from './features/pages/brands/brands.component';
+import { NotfoundComponent } from './features/pages/notfound/notfound.component';
+import { RegisterComponent } from './core/auth/pages/register/register.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "register", pathMatch: "full" },
+    { path: "register", component: RegisterComponent, title: "Register" },
+    { path: "login", component: LoginComponent, title: "Login" },
+    { path: "home", component: HomeComponent, title: "Home" },
+    { path: "cart", component: CartComponent, title: "Cart" },
+    { path: "products", component: ProductsComponent, title: "Products" },
+    { path: "categories", component: CategoriesComponent, title: "Categories" },
+    { path: "brands", component: BrandsComponent, title: "Brands" },
+
+
+
+    { path: "**", component: NotfoundComponent, title: "Not Found" }
+
+];
