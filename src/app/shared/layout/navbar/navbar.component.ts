2c97342461f27d9e5a2b722e9c0523b5f9bd28c7
maiallam57 <maimallam57@gmail.com>
2024-08-30 22:58:11 +0300

    userNavbar

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +10,22 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  private readonly _authService = inject(AuthService);
+  private readonly _platform = inject(PLATFORM_ID);
+
+  loggedUser: boolean = false;
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this._platform)) {
+      this._authService.saveUserData();
+      if (this._authService.userData) {
+        this.loggedUser = true;
+      } else {
+        this.loggedUser = false;
+      }
+    }
+  }
 
 }
